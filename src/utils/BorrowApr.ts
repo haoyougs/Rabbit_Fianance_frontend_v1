796cdc0409cd6eb10e33790e6775265a424ce59b
zhouyangbin <2370108782@qq.com>
2022-05-24 21:26:02 +0800

    edit BorrowApr.ts

--- src/utils/BorrowApr.ts
+++ src/utils/BorrowApr.ts
@@ -2,17 +2,17 @@ export const borrowAprCommon = (BorrowedValue: any, DepositValue: any) => {
     const u = parseFloat(BorrowedValue) / DepositValue;
     let m = 0;
     let b = 0;
-    if (0 < u && u <= 0.8) {
-        m = 0.125;
+    if (0 < u && u <= 0.75) {
+        m = 0.267;
         b = 0;
     }
-    if (0.8 < u && u <= 0.9) {
+    if (0.75 < u && u <= 0.9) {
         m = 0;
-        b = 0.1;
+        b = 0.2;
     }
     if (0.9 < u && u <= 1) {
-        m = 5;
-        b = -4.4;
+        m = 13;
+        b = -11.5;
     }
     const Borrow_Apr = (m * u + b);
     return Borrow_Apr;
